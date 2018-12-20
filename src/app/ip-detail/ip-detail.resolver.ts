16a3fb98d20eb6d8e1d48448005ff8034859db44
David Coelho <davidcoelho058@gmail.com>
2018-12-20 16:27:19 -0600

    replace 2 spaces to 4

--- src/app/ip-detail/ip-detail.resolver.ts
+++ src/app/ip-detail/ip-detail.resolver.ts
@@ -5,23 +5,23 @@ import { IpsService } from '../services/ips.service';
 @Injectable()
 export class IpDetailResolver implements Resolve<any> {
 
-  constructor(
-    private ipsService: IpsService
-  ) { }
+    constructor(
+        private ipsService: IpsService
+    ) { }
 
-  resolve(route: ActivatedRouteSnapshot) {
-    let ip = route.paramMap.get('ipaddress');
-    return new Promise((resolve, reject) => {
-      let ipDetail = this.ipsService.getIpDetail(ip)
-        .then(
-          data => {
-            return resolve(data.ipDetail);
-          },
-          err => {
-            console.log(err)
-            return resolve(null)
-          }
-        )
-    });
-  }
+    resolve(route: ActivatedRouteSnapshot) {
+        let ip = route.paramMap.get('ipaddress');
+        return new Promise((resolve, reject) => {
+            let ipDetail = this.ipsService.getIpDetail(ip)
+                .then(
+                    data => {
+                        return resolve(data.ipDetail);
+                    },
+                    err => {
+                        console.log(err)
+                        return resolve(null)
+                    }
+                )
+        });
+    }
 }
